645024a4bef2ea739eef52d8ec8587194badf8e6
Stefan Fleck <stefan.b.fleck@gmail.com>
2020-01-18 23:29:05 +0100

    cleanup

--- tests/testthat/test_coord_matrix.R
+++ tests/testthat/test_coord_matrix.R
@@ -2,7 +2,6 @@ context("coord_matrix")
 
 
 test_that("as_coord_matrix.matrix works as expected", {
-
   coord_cols <- c("lon", "lat")
   x <- matrix(c(1, 2, 3, 4, 5, 6), ncol = 2)
 
@@ -25,8 +24,8 @@ test_that("as_coord_matrix.matrix works as expected", {
   colnames(x) <- c("lat", "lon")
   expect_identical(guess_loncol(as.data.frame(x)), 2L)
   expect_identical(guess_latcol(as.data.frame(x)), 1L)
-
 })
+
 
 
 
@@ -45,13 +44,9 @@ test_that("as_coord_matrix.numeric works as expected", {
 
 
 
-
 test_that("as_coord_matrix.data.frame works as expected", {
-
-  coord_cols <- c("lon", "lat")
   x <- matrix(c(1, 2, 3, 4, 5, 6), ncol = 2)
   y <- as.data.frame(x)
-
   expect_error(as_coord_matrix(y), class = "cannotGuessColumnError")
 
   # switch lat and lon for the sake of this test
@@ -59,7 +54,7 @@ test_that("as_coord_matrix.data.frame works as expected", {
   colnames(y) <- c("lat", "lon")
   y <- as_coord_matrix(y)
 
-  expect_identical(colnames(y), coord_cols)
+  expect_identical(colnames(y), c("lon", "lat"))
 
   expect_identical(x[, "lat"], y[, "lon"])
   expect_identical(x[, "lon"], y[, "lat"])
