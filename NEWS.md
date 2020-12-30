cdfbfedc81ef82c11df58360dbffffc36c75f143
Stefan Fleck <stefan.fleck@statistik.gv.at>
2020-12-30 11:16:13 +0100

    add unsf

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,7 @@
-smartmap 0.1.1
+smartmap (dev)
 
+* add `unsf()`
+
+smartmap 0.1.1
 
 * first CRAN release
--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,22 @@ backtick <- function(x){
 
 
 
-commalist <- function(x){
-  paste(x, collapse = ", ")
+#' Title
+#'
+#' @param x
+#' @param coord_names
+#'
+#' @return
+#' @export
+#'
+#' @examples
+unsf <- function(
+  x,
+  coord_names = c("lon", "lat")
+){
+  assert(sf::st_geometry_type(x) == "POINT")
+  coords <- sf::st_coordinates(x)
+  res <- cbind(x, coords)
+  sf::st_geometry(res) <- NULL
+  res
 }
