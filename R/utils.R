d98fc4542e56a4911504dcc49c85f72b720285f3
Stefan Fleck <stefan.fleck@statistik.gv.at>
2021-06-21 10:51:34 +0200

    fix broken assertion in unsf

--- R/utils.R
+++ R/utils.R
@@ -18,8 +18,9 @@ unsf <- function(
   x,
   coord_names = c("lon", "lat")
 ){
-  assert(sf::st_geometry_type(x) == "POINT")
+  assert(all(sf::st_geometry_type(x) == "POINT"))
   coords <- sf::st_coordinates(x)
+  colnames(coords) <- coord_names
   res <- cbind(x, coords)
   sf::st_geometry(res) <- NULL
   res
